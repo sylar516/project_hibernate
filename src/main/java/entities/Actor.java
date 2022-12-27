81ae736cfcc50623de76f6a8d406e1cb986fc780
Eldar <ElMAkhmadeev@Greenatom.ru>
2022-12-27 22:14:00 +0300

    добавление полей и аннотаций в класс Actor

--- src/main/java/entities/Actor.java
+++ src/main/java/entities/Actor.java
@@ -1,4 +1,25 @@
 package entities;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "actor", schema = "movie")
 public class Actor {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "actor_id")
+    private Short id;
+
+    @Column(name = "first_name", length = 45, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 45, nullable = false)
+    private String lastName;
+
+    @UpdateTimestamp
+    @Column(name = "last_update", nullable = false)
+    private LocalDateTime lastUpdate;
 }
