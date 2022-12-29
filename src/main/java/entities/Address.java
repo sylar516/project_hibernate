b63f7cdc7c2d06d2d09186bc5345d58c4d0f5347
Eldar <ElMAkhmadeev@Greenatom.ru>
2022-12-29 21:13:25 +0300

    добавление полей и аннотаций в класс Address

--- src/main/java/entities/Address.java
+++ src/main/java/entities/Address.java
@@ -1,4 +1,37 @@
 package entities;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "address", schema = "movie")
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "address_id")
+    private Short id;
+
+    @Column(name = "address", length = 50, nullable = false)
+    private String address;
+
+    @Column(name = "address2", length = 50)
+    private String address2;
+
+    @Column(name = "district", length = 20, nullable = false)
+    private String district;
+
+    @Column(name = "city_id", nullable = false)
+    private Short cityId;
+
+    @Column(name = "postal_code", length = 10)
+    private String postalCode;
+
+    @Column(name = "phone", length = 20, nullable = false)
+    private String phone;
+
+    @UpdateTimestamp
+    @Column(name = "last_update", nullable = false)
+    private LocalDateTime lastUpdate;
 }
