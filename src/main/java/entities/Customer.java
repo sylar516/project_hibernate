8026348f66034fe77e3f5b3c6a8006d6cccc1a65
Eldar <ElMAkhmadeev@Greenatom.ru>
2022-12-29 23:56:53 +0300

    добавление полей и аннотаций в класс Customer добавление поля id в класс Store для устранения ошибки в классе Customer, у которого есть зависимость на Store

--- src/main/java/entities/Customer.java
+++ src/main/java/entities/Customer.java
@@ -1,4 +1,104 @@
 package entities;
 
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "customer", schema = "movie")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id")
+    private Short id;
+
+    @ManyToOne
+    @JoinColumn(name = "store_id", nullable = false)
+    private Store store;
+
+    @Column(name = "first_name", length = 45, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 45, nullable = false)
+    private String lastName;
+
+    @Column(name = "email", length = 50)
+    private String email;
+
+    @OneToOne
+    @JoinColumn(name = "address_id", nullable = false)
+    private Address address;
+
+    @Column(name = "active", nullable = false, columnDefinition = "true")
+    private Boolean active;
+
+    @CreationTimestamp
+    @Column(name = "create_date", nullable = false)
+    private LocalDateTime createDate;
+
+    @UpdateTimestamp
+    @Column(name = "last_update")
+    private LocalDateTime lastUpdate;
+
+    public Short getId() {
+        return id;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public Boolean getActive() {
+        return active;
+    }
+
+    public void setActive(Boolean active) {
+        this.active = active;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
 }
--- src/main/java/entities/Store.java
+++ src/main/java/entities/Store.java
@@ -1,4 +1,12 @@
 package entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "store", schema = "movie")
 public class Store {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "store_id")
+    private Byte id;
 }
