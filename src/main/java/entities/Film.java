17a54577abb85d15ae0f960986382d0e9d209ba6
Eldar <ElMAkhmadeev@Greenatom.ru>
2023-01-07 21:09:17 +0300

    добавление полей и аннотаций в класс Film (кроме поля special_features) добавление поля id в класс Language для устранения ошибки в классе Film, у которого есть зависимость на Store

--- src/main/java/entities/Film.java
+++ src/main/java/entities/Film.java
@@ -1,4 +1,53 @@
 package entities;
 
+import enums.Rating;
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "film", schema = "movie")
 public class Film {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "film_id")
+    private Short id;
+
+    @Column(name = "title", length = 128, nullable = false)
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "release_year")
+    private Short releaseYear;
+
+    @ManyToOne
+    @JoinColumn(name = "language_id", nullable = false)
+    private Language language;
+
+    //как указать поле original_language_id?
+
+    @Column(name = "rental_duration", columnDefinition = "3", nullable = false)
+    private Byte rentalDuration;
+
+    @Column(name = "rental_rate", columnDefinition = "4.99")
+    private BigDecimal rentalRate;
+
+    @Column(name = "length")
+    private Short length;
+
+    @Column(name = "replacement_cost", columnDefinition = "19.99", nullable = false)
+    private BigDecimal replacementCost;
+
+    @Column(name = "rating", columnDefinition = "G")
+    private Rating rating;
+
+    //поле special_features
+
+    @UpdateTimestamp
+    @Column(name = "last_update", nullable = false)
+    private LocalDateTime lastUpdate;
 }
--- src/main/java/entities/Language.java
+++ src/main/java/entities/Language.java
@@ -1,4 +1,11 @@
 package entities;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "language", schema = "movie")
 public class Language {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Byte id;
 }
